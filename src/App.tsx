8afb22bbd7f9dac8c06ec96e57ad0064f7a8aed6
ocinpp <ocin25@gmail.com>
2025-01-05 21:07:15 +0800

    Add new shapes

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,51 @@ import { ObstacleShape } from './components/types'
 export default function App() {
   // Define obstacle shapes
   const obstacleShapes: ObstacleShape[] = [
-    // Square (3 sides)
+    // Square (3x3)
     [
-      { x: 0, y: 0 },
-      { x: 1, y: 0 },
+      { x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 },
+      { x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 },
+      { x: 0, y: 2 }, { x: 1, y: 2 }, { x: 2, y: 2 },
+    ],
+    // L shape (5x5)
+    [
+      { x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }, { x: 0, y: 3 }, { x: 0, y: 4 },
+      { x: 1, y: 4 }, { x: 2, y: 4 }, { x: 3, y: 4 }, { x: 4, y: 4 },
+    ],
+    // T shape (5x5)
+    [
+      { x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 }, { x: 3, y: 0 }, { x: 4, y: 0 },
+      { x: 2, y: 1 }, { x: 2, y: 2 }, { x: 2, y: 3 }, { x: 2, y: 4 },
+    ],
+    // Cross shape (5x5)
+    [
       { x: 2, y: 0 },
-      { x: 0, y: 1 },
-      { x: 0, y: 2 },
       { x: 2, y: 1 },
-      { x: 2, y: 2 },
+      { x: 0, y: 2 }, { x: 1, y: 2 }, { x: 2, y: 2 }, { x: 3, y: 2 }, { x: 4, y: 2 },
+      { x: 2, y: 3 },
+      { x: 2, y: 4 },
     ],
-    // L shape
+    // Z shape (5x5)
     [
-      { x: 0, y: 0 },
-      { x: 0, y: 1 },
-      { x: 0, y: 2 },
-      { x: 1, y: 2 },
-      { x: 2, y: 2 },
+      { x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 },
+      { x: 2, y: 1 }, { x: 2, y: 2 },
+      { x: 2, y: 3 }, { x: 3, y: 3 }, { x: 4, y: 3 },
     ],
-    // T shape
+    // A shape (5x5)
     [
-      { x: 0, y: 0 },
-      { x: 1, y: 0 },
       { x: 2, y: 0 },
-      { x: 1, y: 1 },
-      { x: 1, y: 2 },
+      { x: 1, y: 1 }, { x: 3, y: 1 },
+      { x: 0, y: 2 }, { x: 1, y: 2 }, { x: 2, y: 2 }, { x: 3, y: 2 }, { x: 4, y: 2 },
+      { x: 0, y: 3 }, { x: 4, y: 3 },
+      { x: 0, y: 4 }, { x: 4, y: 4 },
+    ],
+    // M shape (5x5)
+    [
+      { x: 0, y: 0 }, { x: 4, y: 0 },
+      { x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }, { x: 3, y: 1 }, { x: 4, y: 1 },
+      { x: 0, y: 2 }, { x: 2, y: 2 }, { x: 4, y: 2 },
+      { x: 0, y: 3 }, { x: 4, y: 3 },
+      { x: 0, y: 4 }, { x: 4, y: 4 },
     ],
   ];
 
